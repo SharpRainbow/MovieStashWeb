d8c9a2dbf23e4cac7c88ca44ebd11c3903dfc55f
Alexey Panin <panin.lesha2112@yandex.ru>
2024-12-02 11:06:21 +0300

    Mobile design added + fixes

--- home/home.js
+++ home/home.js
@@ -1,37 +1,43 @@
-const slider = document.querySelector('.slider');
-const leftButton = document.querySelector('.scroll-button.left');
-const rightButton = document.querySelector('.scroll-button.right');
+const slider = document.getElementById("slider-col")
+const leftButton = document.getElementById("left-content")
+const rightButton = document.getElementById("right-content")
 
 // Set up the scroll functionality for the left and right buttons
 leftButton.addEventListener('click', function() {
     slider.scrollBy({
-        left: -(slider.firstChild + 10), // Adjust this value based on the width of your slides
+        left: -200, // Adjust this value based on the width of your slides
         behavior: 'smooth'
     });
 });
 
 rightButton.addEventListener('click', function() {
     slider.scrollBy({
-        left: (slider.firstChild + 10), // Adjust this value based on the width of your slides
+        left: 200, // Adjust this value based on the width of your slides
         behavior: 'smooth'
     });
 });
 
-slider.addEventListener('scroll', () => {
-    if (slider.scrollLeft === 0) {
-        leftButton.style.display = 'none';
-    } else {
-        leftButton.style.display = 'block';
-    }
-    if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
-        rightButton.style.display = 'none';
-    } else {
-        rightButton.style.display = 'block';
-    }
-});
+// slider.addEventListener('scroll', () => {
+//     if (slider.scrollLeft === 0) {
+//         leftButton.style.display = 'none';
+//     } else {
+//         leftButton.style.display = 'block';
+//     }
+//     if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+//         rightButton.style.display = 'none';
+//     } else {
+//         rightButton.style.display = 'block';
+//     }
+// });
 
 const nodes = document.querySelectorAll('.image-container');
 
 nodes.forEach(node => {
     [...Array(10)].forEach(_ => node.parentNode.insertBefore(node.cloneNode(true), node));
+});
+
+const node = document.querySelectorAll('.collection-container');
+
+node.forEach(node => {
+    [...Array(15)].forEach(_ => node.parentNode.insertBefore(node.cloneNode(true), node));
 });
--- menu/script.js
+++ menu/script.js
@@ -2,50 +2,167 @@ document.addEventListener("DOMContentLoaded", () => {
     const navItems = document.querySelectorAll('.nav-item');
     const mainContent = document.getElementById('main-content');
 
+    window.onpopstate = function (event) {
+        if (event.state && event.state.content) {
+            // Restore the content from history state
+            loadContent(event.state.content);
+        }
+    };
+
+    function updateButtonVisibility(slider, left, right) {
+        if (slider.scrollWidth > slider.clientWidth) {
+            if (slider.scrollLeft === 0) {
+                left.style.display = 'none';
+            } else {
+                left.style.display = 'flex';
+            }
+            if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+                right.style.display = 'none';
+            } else {
+                right.style.display = 'flex';
+            }
+        } else {
+            left.style.display = "none";
+            right.style.display = "none";
+        }
+    }
+
     function loadScripts(url) {
         if (url.includes("home")) {
-            const slider = document.querySelector('.slider');
-            const item = document.querySelector('.image-container');
-            const leftButton = document.querySelector('.scroll-button.left');
-            const rightButton = document.querySelector('.scroll-button.right');
-
+            //const nodes = document.querySelectorAll('.image-container');
+            //nodes.forEach(node => {
+            //    [...Array(10)].forEach(_ => node.parentNode.insertBefore(node.cloneNode(true), node));
+            //});
+            const container = document.getElementById("slider-content");
+            [...Array(10)].forEach((value, index) => {
+                const newElement = document.createElement('div');
+                newElement.classList.add('image-container');
+                // Присваиваем уникальный ID
+                newElement.id = `item-${index + 1}`; // Используем индекс для уникальности
+                const img = document.createElement('img');
+                img.src = "/home/paceholder.jpg";
+                img.alt = `desc${index+1}`;
+                newElement.appendChild(img);
+                newElement.setAttribute("data-page", "/content/content.html")
+                newElement.setAttribute("data-id", (index+1).toString())
+                container.appendChild(newElement);
+            })
+            const sliderContent = document.getElementById('slider-content');
+            const leftButton = document.getElementById('left-content');
+            const rightButton = document.getElementById('right-content');
             leftButton.addEventListener('click', function() {
-                slider.scrollBy({
-                    left: -(item.clientWidth + 10), // Adjust this value based on the width of your slides
+                sliderContent.scrollBy({
+                    left: -(items[0].clientWidth + 10),
                     behavior: 'smooth'
                 });
             });
 
             rightButton.addEventListener('click', function() {
-                slider.scrollBy({
-                    left: (item.clientWidth + 10), // Adjust this value based on the width of your slides
+                sliderContent.scrollBy({
+                    left: (items[0].clientWidth + 10),
                     behavior: 'smooth'
                 });
             });
 
-            slider.addEventListener('scroll', () => {
-                if (slider.scrollLeft === 0) {
+            const items = document.querySelectorAll('.image-container');
+            items.forEach(item => item.addEventListener('click', function() {
+                const page = item.getAttribute('data-page');
+                const id = item.getAttribute('data-id');
+                console.log(id)
+                if (page) {
+                    navItems.forEach(nav => nav.classList.remove('active'));
+                    loadContent(page, id);
+                }
+            }))
+
+            sliderContent.addEventListener('scroll', () => {
+                if (sliderContent.scrollLeft === 0) {
                     leftButton.style.display = 'none';
                 } else {
-                    leftButton.style.display = 'block';
+                    leftButton.style.display = 'flex';
                 }
-                if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+                if (sliderContent.scrollLeft + sliderContent.clientWidth >= sliderContent.scrollWidth) {
                     rightButton.style.display = 'none';
                 } else {
-                    rightButton.style.display = 'block';
+                    rightButton.style.display = 'flex';
                 }
             });
 
-            const nodes = document.querySelectorAll('.image-container');
-
-            nodes.forEach(node => {
+            const colNodes = document.querySelectorAll('.collection-container');
+            colNodes.forEach(node => {
                 [...Array(10)].forEach(_ => node.parentNode.insertBefore(node.cloneNode(true), node));
             });
+            const sliderCol = document.getElementById('slider-col');
+            const leftButtonCol = document.getElementById('left-col');
+            const rightButtonCol = document.getElementById('right-col');
+            leftButtonCol.addEventListener('click', function() {
+                sliderCol.scrollBy({
+                    left: -(colNodes[0].clientWidth + 10),
+                    behavior: 'smooth'
+                });
+            });
+
+            rightButtonCol.addEventListener('click', function() {
+                sliderCol.scrollBy({
+                    left: (colNodes[0].clientWidth + 10),
+                    behavior: 'smooth'
+                });
+            });
+
+            sliderCol.addEventListener('scroll', () => {
+                if (sliderCol.scrollLeft === 0) {
+                    leftButtonCol.style.display = 'none';
+                } else {
+                    leftButtonCol.style.display = 'flex';
+                }
+                if (sliderCol.scrollLeft + sliderCol.clientWidth >= sliderCol.scrollWidth) {
+                    rightButtonCol.style.display = 'none';
+                } else {
+                    rightButtonCol.style.display = 'flex';
+                }
+            });
+        }
+        else if (url.includes("content")) {
+            const id = window.location.pathname.substring(window.location.pathname.lastIndexOf("/"))
+            console.log(id);
+            const nodes = document.querySelectorAll('.person-container');
+            nodes.forEach(node => {
+               [...Array(15)].forEach(_ => node.parentNode.insertBefore(node.cloneNode(true), node));
+            });
+            const slider = document.querySelector(".slider");
+            const leftButton = document.querySelector(".scroll-button.left");
+            const rightButton = document.querySelector(".scroll-button.right");
+
+            leftButton.addEventListener('click', function() {
+                slider.scrollBy({
+                    left: -(nodes[0].clientWidth + 10),
+                    behavior: 'smooth'
+                });
+            });
+            rightButton.addEventListener('click', function() {
+                slider.scrollBy({
+                    left: (nodes[0].clientWidth + 10),
+                    behavior: 'smooth'
+                });
+            });
+
+            setTimeout(updateButtonVisibility, 10, slider, leftButton, rightButton);
+            window.addEventListener("resize", () => {
+                updateButtonVisibility(slider, leftButton, rightButton);
+            });
+            slider.addEventListener('scroll', () => {
+                updateButtonVisibility(slider, leftButton, rightButton)
+            });
+
+            const rateBtn = document.getElementById("rate")
+            rateBtn.addEventListener('click', function() {
+                const dialog = document.getElementById('rate-dialog');
+                dialog.show()
+            })
         }
     }
 
-    // Function to load content via AJAX
-    function loadContent(page) {
+    function loadContent(page, id = "0") {
         fetch(page)
             .then(response => {
                 if (!response.ok) {
@@ -54,42 +171,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 return response.text();
             })
             .then(data => {
-                // Insert the new content into the main content area
-                // mainContent.innerHTML = data;
-                // var scripts = mainContent.querySelectorAll('script');
-                // if (scripts !== null && scripts.length > 0) {
-                //     var loadScript = index => {
-                //         if (index < scripts.length) {
-                //             var newScript = document.createElement('script');
-                //             if (scripts[index].innerText) {
-                //                 var inlineScript = document.createTextNode(scripts[index].innerText);
-                //                 newScript.appendChild(inlineScript);
-                //             }
-                //             else {
-                //                 newScript.src = scripts[index].src;
-                //             }
-                //             scripts[index].parentNode.removeChild(scripts[index]);
-                //             newScript.addEventListener('load', event => loadScript(index + 1));
-                //             newScript.addEventListener('error', event => loadScript(index + 1));
-                //             mainContent.appendChild(newScript);
-                //         }
-                //     }
-                //     loadScript(0)
-                // }
-                // for (var i = 0; i < scripts.length; i++) {
-                //     if (scripts[i].innerText) {
-                //         eval(scripts[i].innerText);
-                //     } else {
-                //         fetch(scripts[i].src).then(function(data) {
-                //             data.text().then(function(r) {
-                //                 eval(r);
-                //             })
-                //         })
-                //     }
-                // }
                 const parser = new DOMParser();
                 const doc = parser.parseFromString(data, 'text/html');
                 mainContent.innerHTML = doc.querySelector('body').innerHTML;
+                if (page.includes("home"))
+                    history.pushState({ content: page }, "", "/home")
+                else if (page.includes("content"))
+                    history.pushState({ content: page }, "", `/content/${id}`)
             })
             .then(() => {
                 loadScripts(page);
@@ -97,44 +185,23 @@ document.addEventListener("DOMContentLoaded", () => {
             .catch(error => console.error('Error loading page:', error));
     }
 
-    // Event listener for each navigation item
     navItems.forEach(item => {
         item.addEventListener('click', () => {
-            // Get the data-page attribute, which specifies the HTML file to load
             const page = item.getAttribute('data-page');
-            console.log(page)
             if (page) {
-                // Remove 'active' class from all items and set it on the clicked one
                 navItems.forEach(nav => nav.classList.remove('active'));
                 item.classList.add('active');
-
-                // Load the specified page content
                 loadContent(page);
-                // jQuery(document).ready(function (){
-                //     let cont = jQuery('#main-content');
-                //     cont.load(page, function(response, status, xhr) {
-                //         if (status === 'error') {
-                //             console.error(xhr.status + ': ' + xhr.statusText);
-                //         }
-                //     });
-                // });
             }
         });
     });
-    loadContent("/home/home.html");
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Select all navigation items
-    const navItems = document.querySelectorAll('.nav-item');
-
-    // Add a click event listener to each nav item
-    navItems.forEach(item => {
-        item.addEventListener('click', function () {
-            // Remove 'active' class from all items
-            navItems.forEach(i => i.classList.remove('active'));
-            // Add 'active' class to the clicked item
-            this.classList.add('active');
-        });
-    });
+    const initialPage = window.location.pathname || 'home';
+    var path = ""
+    if (initialPage.includes("menu") || initialPage.includes("home") || initialPage === "/") {
+        path = "/home/home.html";
+    }
+    else if (initialPage.includes("content")) {
+        path = "/content/content.html";
+    }
+    loadContent(path);
 });
